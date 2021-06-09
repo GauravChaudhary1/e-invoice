6cde3ab83e7683672aa31277c6f485f2faa6328d
Gauravchaudhary1 <gauravchaudhary.ait@gmail.com>
2021-06-09 10:35:26 +0530

    Invoice

--- src/app/main/invoice/invoice.component.ts
+++ src/app/main/invoice/invoice.component.ts
@@ -1,11 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+interface invoice{
+  invoiceNumber : string | number;
+  billto : string | number;
+  customer : string | number;
+  invoicedate : string;
+}
+
 @Component({
   selector: 'app-invoice',
   templateUrl: './invoice.component.html',
   styleUrls: ['./invoice.component.scss']
 })
 export class InvoiceComponent implements OnInit {
+
+  invoices = [
+    {invoiceNumber : 1, billto:1, customer: 'Gaurav Industries Ltd', invoicedate: '31.12.9999' },
+    {invoiceNumber : 1, billto:1, customer: 'Gaurav Industries Ltd', invoicedate: '31.12.9999' },
+    {invoiceNumber : 1, billto:1, customer: 'Gaurav Industries Ltd', invoicedate: '31.12.9999' },
+    {invoiceNumber : 1, billto:1, customer: 'Gaurav Industries Ltd', invoicedate: '31.12.9999' },
+    {invoiceNumber : 1, billto:1, customer: 'Gaurav Industries Ltd', invoicedate: '31.12.9999' },
+    {invoiceNumber : 1, billto:1, customer: 'Gaurav Industries Ltd', invoicedate: '31.12.9999' }
+  ]
 
   constructor() { }
 
